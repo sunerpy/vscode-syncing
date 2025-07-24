51cf5b178d0d3e88914a3860c3dbbfa4b3a20a90
sunerpy <nkuzhangshn@gmail.com>
2025-07-24 16:14:13 +0800

    fix: 修复 pnpm 版本并优化 Gist 导出日志

--- src/syncManager.ts
+++ src/syncManager.ts
@@ -709,9 +709,7 @@ export class SyncManager {
       fileName,
       JSON.stringify(data, null, 2),
     );
-    this.outputChannel.appendLine(
-      `Gist导出成功: ${gist.html_url} resp: ${JSON.stringify(gist, null, 2)}`,
-    );
+    this.outputChannel.appendLine(`Gist导出成功: ${gist.html_url}`);
     return gist.html_url;
   }
 
